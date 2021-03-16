98b45ff99a1951c31688ca8eca354f13e0dcf3cb
Simo Aleksandrov <simo3003@me.com>
2021-03-16 17:36:25 +0200

    wip render piano

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/fr3fou/goda
 go 1.15
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/fr3fou/gusic v0.0.0-20201017164734-72a7e32d5644
 	github.com/gen2brain/raylib-go v0.0.0-20210227124741-9d258bad6516
 )
--- main.go
+++ main.go
@@ -8,18 +8,13 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-const (
-	maxSamples          = 48000 * 5
-	maxSamplesPerUpdate = 4096
-)
-
 type Key struct {
-	Color rl.Color
-	Note  gusic.Note
+	gusic.Note
+	IsSemitone bool
 }
 
-func NewKey(rect rl.Rectangle, note gusic.Note) *Key {
-	return &Key{Note: note}
+func NewKey(note gusic.Note, isSemitone bool) Key {
+	return Key{Note: note, IsSemitone: isSemitone}
 }
 
 func (p *Key) Samples() []float32 {
@@ -36,24 +31,25 @@ func (p *Key) Samples() []float32 {
 }
 
 func main() {
-	rl.InitWindow(1650, 900, "goda - a simple music pad")
+	width := int32(1680)
+	height := int32(900)
+	rl.InitWindow(width, height, "goda - a simple music pad")
 
 	rl.InitAudioDevice()
-	defer rl.CloseAudioDevice() // Close audio device (music streaming is automatically stopped)
+	defer rl.CloseAudioDevice()
 
-	// Init raw audio stream (sample rate: 22050, sample size: 32bit-float, channels: 1-mono)
 	stream := rl.InitAudioStream(48000, 32, 1)
-	defer rl.CloseAudioStream(stream) // Close raw audio stream and delete buffers from RAM
+	defer rl.CloseAudioStream(stream)
 
-	// Fill audio stream with some samples (sine wave)
-	data := make([]float32, maxSamples)
+	// maxSamples := 48000 * 5
+	// maxSamplesPerUpdate := 4096
 
-	// NOTE: The generated MAX_SAMPLES do not fit to close a perfect loop
-	// for that reason, there is a clip everytime audio stream is looped
+	// data := make([]float32, maxSamples)
+
 	rl.PlayAudioStream(stream)
 
-	totalSamples := int32(0)
-	samplesLeft := int32(totalSamples)
+	// totalSamples := int32(0)
+	// samplesLeft := int32(totalSamples)
 
 	bpm := 200
 	noteLength := 4
@@ -66,46 +62,78 @@ func main() {
 	// semiquaver := semibreve / 16
 	// demisemiquaver := semibreve / 32
 
+	volume := 0.125
+
+	keys := []Key{}
+	whiteKeys := []Key{}
+	startOctave := 3
+	lastOctave := 5
+	octaveCount := lastOctave - startOctave + 1 // +1 because it's inclusive
+
+	// whiteWidth := 70
+	whiteWidth := int(width / (int32(octaveCount) * 7)) // 7 is white keys per octave
+	blackWidth := int(0.6 * float64(whiteWidth))
+
+	topMargin := 200
+
+	for i := startOctave; i <= lastOctave; i++ {
+		// TODO: set duration to 0 and update it based on hold duration
+		keys = append(keys,
+			NewKey(gusic.C(i, quaver, volume), false),
+			NewKey(gusic.CS(i, quaver, volume), true),
+			NewKey(gusic.D(i, quaver, volume), false),
+			NewKey(gusic.DS(i, quaver, volume), true),
+			NewKey(gusic.E(i, quaver, volume), false),
+			NewKey(gusic.F(i, quaver, volume), false),
+			NewKey(gusic.FS(i, quaver, volume), true),
+			NewKey(gusic.G(i, quaver, volume), false),
+			NewKey(gusic.GS(i, quaver, volume), true),
+			NewKey(gusic.A(i, quaver, volume), false),
+			NewKey(gusic.AS(i, quaver, volume), true),
+			NewKey(gusic.B(i, quaver, volume), false),
+		)
+	}
+
+	for _, key := range keys {
+		if !key.IsSemitone {
+			whiteKeys = append(whiteKeys, key)
+		}
+	}
+
 	rl.SetTargetFPS(60)
 
-	color := rl.Yellow
 	for !rl.WindowShouldClose() {
 		// Refill audio stream if required
 		if rl.IsAudioStreamProcessed(stream) {
-			numSamples := int32(0)
-			if samplesLeft >= maxSamplesPerUpdate {
-				numSamples = maxSamplesPerUpdate
-			} else {
-				numSamples = samplesLeft
-			}
+			// numSamples := int32(0)
+			// if samplesLeft >= maxSamplesPerUpdate {
+			// 	numSamples = maxSamplesPerUpdate
+			// } else {
+			// 	numSamples = samplesLeft
+			// }
 
-			rl.UpdateAudioStream(stream, data[totalSamples-samplesLeft:], numSamples)
+			// rl.UpdateAudioStream(stream, data[totalSamples-samplesLeft:], numSamples)
 
-			samplesLeft -= numSamples
+			// samplesLeft -= numSamples
 		}
 
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
+		rl.ClearBackground(rl.Yellow)
 		if rl.IsMouseButtonReleased(rl.MouseLeftButton) {
-			coords := rl.GetMousePosition()
-			if coords.X >= 100 && coords.Y >= 100 && coords.X <= 600+100 && coords.Y <= 600+100 {
-				note := gusic.D(4, quaver, 0.125)
-				samples := samplesToFloat32(
-					note.Samples(
-						// TODO, configurable params
-						48000,
-						math.Sin,
-						gusic.NewLinearADSR(
-							gusic.NewRatios(0.25, 0.25, 0.25, 0.25), 1.35, 0.35,
-						),
-					),
-				)
-				copy(data, samples)
-				totalSamples = int32(len(samples))
-				samplesLeft = totalSamples
+			// coords := rl.GetMousePosition()
+		}
+
+		for i := range whiteKeys {
+			rl.DrawRectangle(int32(i*whiteWidth), int32(topMargin), int32(whiteWidth), height-int32(topMargin), rl.White)
+			rl.DrawRectangle(int32(i*whiteWidth), int32(topMargin), 2, height-int32(topMargin), rl.Gray)
+		}
+
+		for i, key := range keys {
+			if key.IsSemitone {
+				rl.DrawRectangle(int32(i*blackWidth), int32(topMargin), int32(blackWidth), int32(0.6*float32(height-int32(topMargin))), rl.Black)
 			}
 		}
-		rl.DrawRectangle(100, 100, 600, 600, color)
+
 		rl.EndDrawing()
 	}
 
